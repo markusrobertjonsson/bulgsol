1cbfdcefd06136fa09f251dea02e02e3c328cd51
Markus Jonsson <markusrobertjonsson@gmail.com>
2024-07-12 17:44:55 +0200

    Bugfix

--- levelstat.py
+++ levelstat.py
@@ -251,7 +251,7 @@ x0, y0 = 0, 0
 
 with writer.saving(fig, "writer_test.mp4", 100):
     for i in range(100):
-        x0 += 0.1 * np.random.randn()
-        y0 += 0.1 * np.random.randn()
+        x0 += 0.1 * np.random.randn(10)
+        y0 += 0.1 * np.random.randn(10)
         l.set_data(x0, y0)
         writer.grab_frame()
